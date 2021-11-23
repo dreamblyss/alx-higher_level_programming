747778bc89d869aa86f6ebc307e9a933c70efd4b
dreamblyss <iwekuba.henry@gmail.com>
2021-11-23 22:50:37 +0000

    correction

--- 0x01-python-if_else_loops_functions/linked_lists.c
+++ 0x01-python-if_else_loops_functions/linked_lists.c
@@ -10,7 +10,7 @@
 
 size_t print_listint(const listint_t *h)
 {
-       const listint_t * current;
+       const listint_t *current;
              unsigned int n; /* number of nodes */
      
  	     current = h;
@@ -39,7 +39,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 
   listint_t *new;
-  listint_t * current
+  listint_t *current
 
     current = *head;
 

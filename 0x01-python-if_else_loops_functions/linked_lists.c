b15706331ceb8f8ab47ddb18b649ce9448cb68de
dreamblyss <iwekuba.henry@gmail.com>
2021-11-25 14:17:12 +0000

    13-inser-number

--- 0x01-python-if_else_loops_functions/linked_lists.c
+++ 0x01-python-if_else_loops_functions/linked_lists.c
@@ -39,7 +39,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 
   listint_t *new;
-  listint_t *current
+  listint_t *current;
 
     current = *head;
 
--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -2,19 +2,19 @@
 #define LISTS_H
 
 /**
- * * struct listint_s - singly linked list
- * * @n: integer
- * * @next: points to the next node
- * * 
- * * Description: singly linked list node structure
- * * for Holbertob project
- * */
+ *  struct listint_s - singly linked list
+ *  @n: integer
+ *  @next: points to the next node
+ *  
+ *  Description: singly linked list node structure
+ * 
+ */
 
 typedef struct listint_s
 {
 
   int n;
-     struct listint_s *next;
+  struct listint_s *next;
 
 }listint_t;
 
@@ -23,6 +23,6 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
-listint_t *insert_node(listint_t **head, int number)
+listint_t *insert_node(listint_t **head, int number);
 
 #endif /* LISTS_H */
